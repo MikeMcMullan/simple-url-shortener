b739de128cfeae6ad168de1124e027ac027579a6
Mike McMullan <mike@mikemcmullan.me>
2015-12-20 18:34:06 -0500

    Added database migration.

---
+++ database/migrations/2015_12_20_233001_create_urls_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateUrlsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('urls', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('token', 20)->index();
+            $table->string('url')->index();
+            $table->integer('visits')->default(0);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('urls');
+    }
+}
